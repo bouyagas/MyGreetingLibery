58f35f122664c4a54a3d1f21e5e61a9e69e2d4b9
mohamed gassama <bouyagassama@gmail.com>
2017-10-10 23:11:27 -0400

    add new object

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('John', 'Doe');
-
+var g = G$('Mohamed', 'Gassama');
 // use our chainable methods
 g.greet().setLang('es').greet(true).log();
 
